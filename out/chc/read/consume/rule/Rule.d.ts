0788f266bc48d7006a8002d785fedc39d00277e3
ChiriVulpes <github@chiri.works>
2025-07-09 09:42:55 +0000

    Deploy 42f28fe: Lib: %appearance-none, %weight-semibold

--- out/chc/read/consume/rule/Rule.d.ts
+++ out/chc/read/consume/rule/Rule.d.ts
@@ -49,7 +49,7 @@ export interface ChiriComponentStateScheme extends ChiriComponentBase {
     spread: boolean;
     scheme: "dark" | "light";
 }
-export type PseudoName = "before" | "after" | "backdrop" | "range-thumb" | "range-track" | "placeholder" | "details-content" | "swatch" | "swatch-wrapper";
+export type PseudoName = "before" | "after" | "backdrop" | "range-thumb" | "range-track" | "placeholder" | "details-content" | "swatch" | "swatch-wrapper" | "marker";
 export interface ChiriComponentPseudo extends ChiriComponentBase {
     subType: "pseudo";
     spread: boolean;
